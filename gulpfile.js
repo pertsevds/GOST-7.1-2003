a9d2de8c6da23a386a7f49fd4b33ec9c7b9cda25
Dmitriy Pertsev <33601+davaeron@users.noreply.github.com>
2020-02-12 14:11:41 +0300

    working output

--- gulpfile.js
+++ gulpfile.js
@@ -16,14 +16,20 @@ function startWord(cb) {
   return exec('start winword test.docm');
 }
 
-function transform(cb) {
-  return exec('"libxml/xsltproc" -o samples/Bibliorgaphy.xml utils/bibliography.xsl samples/Sources.xml', (err, stdout, stderr) => {
+function transformSourcesToBibliography(cb) {
+  return exec('"libxml/xsltproc" -o samples/Bibliorgaphy.xml utils/bibliography.xsl samples/Sources.xml');
+}
+
+function showBibliographyResult(cb) {
+  exec('"libxml/xsltproc" GOST71.xsl samples/Bibliorgaphy.xml', (err, stdout, stderr) => {
     console.log(stdout);
     console.log(stderr);
     cb();
   });
 }
 
+
 exports.default = series(killWord, copy, startWord)
 exports.install = copy
-exports.transform = transform
+exports.transformSourcesToBibliography = transformSourcesToBibliography
+exports.showBibliographyResult = showBibliographyResult
--- utils/bibliography.xsl
+++ utils/bibliography.xsl
@@ -1,5 +1,5 @@
 <xsl:stylesheet version="1.0" xmlns:xsl="http://www.w3.org/1999/XSL/Transform">
-    <xsl:output method="xml" indent="yes" omit-xml-declaration="yes" />
+    <xsl:output method="xml" indent="yes" />
 
     <xsl:template match="@* | node()">
         <xsl:copy>
