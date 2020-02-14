a11ad161644250a8a8f8b783e995eb2f1a13df26
Dmitriy Pertsev <33601+davaeron@users.noreply.github.com>
2020-02-14 15:55:53 +0300

    WIP

--- gulpfile.js
+++ gulpfile.js
@@ -2,12 +2,12 @@ const { parallel, series, src, dest } = require('gulp');
 const { exec } = require('child_process');
 
 
-function install() {
-  return src('GOST71.xsl').pipe(dest(process.env.APPDATA + '/Microsoft/Bibliography/Style/'));
-}
-
 function copySources() {
   return src(process.env.APPDATA + '/Microsoft/Bibliography/Sources.xml').pipe(dest('sources/'));
+}
+
+function install() {
+  return src('GOST71.xsl').pipe(dest(process.env.APPDATA + '/Microsoft/Bibliography/Style/'));
 }
 
 function installSources() {
@@ -18,18 +18,6 @@ function killWord(cb) {
   exec('taskkill /IM "winword.exe" /F', (err, stdout, stderr) => {
     cb();
   });
-}
-
-function startWord() {
-  return exec('start winword test.docm');
-}
-
-function transformSourcesToBibliography() {
-  return exec('"libxml/xsltproc" -o sources/Bibliorgaphy.xml utility-xslts/bibliography.xsl sources/Sources.xml');
-}
-
-function transformSourcesToCitation() {
-  return exec('"libxml/xsltproc" -o sources/Citation.xml utility-xslts/citation.xsl sources/Sources.xml');
 }
 
 function showBibliographyResult(cb) {
@@ -48,10 +36,22 @@ function showCitationResult(cb) {
   });
 }
 
-exports.default = series(killWord, install, startWord)
+function startWord() {
+  return exec('start winword test.docm');
+}
+
+function transformSourcesToBibliography() {
+  return exec('"libxml/xsltproc" -o sources/Bibliorgaphy.xml utility-xslts/bibliography.xsl sources/Sources.xml');
+}
+
+function transformSourcesToCitation() {
+  return exec('"libxml/xsltproc" -o sources/Citation.xml utility-xslts/citation.xsl sources/Sources.xml');
+}
+
 exports.copySources = copySources
+exports.default = series(killWord, install, startWord)
 exports.install = install
 exports.installSources = installSources
-exports.transformSources = parallel(transformSourcesToBibliography, transformSourcesToCitation)
 exports.showBibliographyResult = showBibliographyResult
 exports.showCitationResult = showCitationResult
+exports.transformSources = parallel(transformSourcesToBibliography, transformSourcesToCitation)
